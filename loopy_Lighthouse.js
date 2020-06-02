ab3d5a4fa7f3f09f2a543c8e0028cab846bcc90f
MargaritaSt <margarita.struts@me.com>
2020-06-02 08:17:49 +0000

    hello.js

---
+++ loopy_Lighthouse.js
@@ -0,0 +1,13 @@
+for (let repeat = 100; repeat <= 200; repeat++) {
+  if (repeat % 4 === 0 && repeat % 3 === 0) {
+    console.log("LoopyLighthouse ");
+  }
+ else if (repeat % 3 === 0) {
+    console.log("Loopy ");
+  }
+  else if (repeat % 4 === 0) {
+    console.log("Lighthouse ");
+  } 
+else {console.log(repeat);}
+}
+
